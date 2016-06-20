8d7e0169926169fbf4a70479c4996465bf255fc1
Martin Moser <moser@emineo.ch>
2016-06-20 22:24:33 +0200

    Initial Commit

---
+++ snow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snow
+//
+//  Created by Martin Moser on 20.06.16.
+//  Copyright (c) 2016 martinmoser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
